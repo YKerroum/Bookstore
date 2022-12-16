37d132ed459ea84cc059ac1634f8d508956bd2d8
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2022-12-16 10:48:39 +0100

    feat: add style to books list buttons

--- src/components/Book.js
+++ src/components/Book.js
@@ -10,14 +10,38 @@ const Book = ({ isLoading, books }) => {
   };
 
   const bookListGenerator = books.map(
-    ({ id, title, author }) => (
+    ({
+      id, title, author, category,
+    }) => (
       <li key={id}>
         <div className="book">
-          <div className="book-info">
-            <div className="book-title">{title}</div>
-            <div className="book-author">{author}</div>
-            <div className="book-buttons">
-              <button type="button" className="book-button" onClick={() => handleDelete(id)}>Remove</button>
+          <div className="book-content">
+            <div className="book-info">
+              <h4 className="book-category">{category}</h4>
+              <h2 className="book-title">{title}</h2>
+              <h6 className="book-author">{author}</h6>
+              <div className="action-buttons">
+                <button className="button-outline" type="button">Comments</button>
+                <div className="vertical-divider" />
+                <button className="button-outline" type="button" onClick={() => handleDelete(id)}>Remove</button>
+                <div className="vertical-divider" />
+                <button className="button-outline" type="button">Edit</button>
+              </div>
+            </div>
+            <div className="progress-container">
+              <div className="circular-progress-container"><div className="circular-progress" /></div>
+              <div className="progress-stat">
+                <p className="percent-complete">64%</p>
+                <p className="completed">Completed</p>
+              </div>
+              <div className="progress-divider" />
+              <div className="current-chapter-container">
+                <div>
+                  <p className="current-chapter-label">CURRENT CHAPTER</p>
+                  <p className="current-chapter">Chapter 17</p>
+                </div>
+                <div><button className="primary-button" type="button">UPDATE PROGRESS</button></div>
+              </div>
             </div>
           </div>
         </div>
